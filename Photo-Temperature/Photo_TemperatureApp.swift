59476363be0fe444fb9b8950b78dc4c59fd38067
auliyamichelle <48114085+Auliya123@users.noreply.github.com>
2025-03-02 22:29:11 +0700

    Initial Commit

---
+++ Photo-Temperature/Photo_TemperatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_TemperatureApp.swift
+//  Photo-Temperature
+//
+//  Created by Auliya Michelle Adhana on 02/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Photo_TemperatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
